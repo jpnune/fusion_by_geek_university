3cb32ee9df5d0d49d77e2e8c44e71ff66d689a20
João Paulo Nunes <jpnune@gmail.com>
2022-09-15 18:14:22 -0300

    feito o models Recuros, Funcionario, Cargo e Serviços

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cargo, Funcionario, Recurso, Servico
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display= ('servico', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_dispaly = ('nome', 'cargo', 'ativo', 'modificado')
+
+@admin.register(Recurso)
+class RecursoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'icone', 'ativo', 'modificado')
--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import TemplateView
 
+from .models import Funcionario, Servico, Recurso
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        context['recursos'] = Recurso.objects.order_by('?').all()
+        return context
+
